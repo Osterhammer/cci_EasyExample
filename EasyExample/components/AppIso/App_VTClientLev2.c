1b12dac1dab398f9045bb0923fa0c36b761912c1
Osterhammer <l1222@schueler.ms-muc.de>
2022-01-11 10:13:31 +0100

    Lösung mit "switch"

--- EasyExample/components/AppIso/App_VTClientLev2.c
+++ EasyExample/components/AppIso/App_VTClientLev2.c
@@ -173,13 +173,18 @@ void VTC_handleSoftkeysAndButtons_RELEASED(const struct ButtonActivation_S *pBut
 	default:
 		break;
 	}
-
-	if(Gesamtzaehler == 1)
+	switch(Gesamtzaehler)
+	{
+	case 1:
 		IsoVtcCmd_CtrlAudioSignal(pButtonData->u8Instance, 1, 440, 100, 400);
-	if(Gesamtzaehler == 2)
+		break;
+	case 2:
 		IsoVtcCmd_CtrlAudioSignal(pButtonData->u8Instance, 2, 440, 100, 400);
-	if(Gesamtzaehler == 3)
+		break;
+	case 3:
 		IsoVtcCmd_CtrlAudioSignal(pButtonData->u8Instance, 3, 440, 100, 400);
+		break;
+	}
 
 	// Senden des Wertes der lokalen Variable Tageszaehler an die NumberVariable_Tageszaehler
 	IsoVtcCmd_NumericValue(pButtonData->u8Instance, NumberVariable_Tageszaehler, Tageszaehler);
